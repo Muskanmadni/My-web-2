b7c1ddbba428d700cb4ed182402c5642064fca45
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2024-09-29 02:22:26 -0700

    first commit

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/header";
+
+export default function About(){
+    return(
+        <div>
+            <Header/>
+            <h1>hello my name is muskan</h1>
+        </div>
+    )
+}
---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/header";
+
+
+export default function Contact(){
+    return(
+        <div>
+            <Header/>
+            <h1>Email : muskanmadni153@Gmail.com
+                <br/>
+                Contact : 03408051451
+            </h1>
+        </div>
+    )
+}
